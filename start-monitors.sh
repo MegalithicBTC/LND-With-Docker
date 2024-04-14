41466ec1e8f5f8489a2894fbab53d0aa833cca93
perls <info@megalithic.me>
2024-04-14 07:50:41 -0700

    no message

--- start-monitors.sh
+++ start-monitors.sh
@@ -1,2 +1,2 @@
-docker compose -f ./pworker/dpworker.yml down upmonitor
-./pworker/write_zpool_status.sh & docker compose -f ./pworker/dren.yml up upmonitor
+docker compose -f ./pworker/dpworker.yml down send-node-status-to-queue
+./pworker/write_zpool_status.sh & docker compose -f ./pworker/dpworker.yml up send-node-status-to-queue
